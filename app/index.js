a735812b2f6d4ff7a432329287683fd86948303a
James Shen <jjss886@gmail.com>
2019-12-27 23:58:10 +0100

    Working board with X & O and move state to parent

--- app/index.js
+++ app/index.js
@@ -5,13 +5,16 @@ import { BrowserRouter as Router } from "react-router-dom";
 import "./style.css";
 
 // ----------------- VARIABLES ----------------- //
-// const size = document.getElementById("num-columns").value;
 let size = 3;
-const startBoard = size => Array(size).fill(Array(size).fill(null));
-const theme = {
-  one: "X",
-  two: "O"
-};
+// const startGame = size => {
+//   return Array(size)
+//     .fill(null)
+//     .map(() => Array(size).fill(null));
+// };
+// const theme = {
+//   one: "X",
+//   two: "O"
+// };
 
 // ----------------- SQUARE ----------------- //
 const Square = props => {
@@ -26,22 +29,18 @@ const Square = props => {
 class Board extends Component {
   constructor() {
     super();
-    this.state = {
-      board: startBoard(size),
-      oneNext: true
-    };
     this.renderSquare = this.renderSquare.bind(this);
     this.renderBoard = this.renderBoard.bind(this);
     this.handleClick = this.handleClick.bind(this);
   }
 
-  renderSquare(i) {
+  renderSquare(i, size) {
     let row = Math.floor(i / size),
       col = i % size;
     return (
       <Square
         key={i}
-        status={this.state.board[row][col]}
+        status={this.props.state.board[row][col]}
         onClick={() => this.handleClick(i)}
       />
     );
@@ -51,7 +50,7 @@ class Board extends Component {
     for (let row = 0; row < size; row++) {
       let children = [];
       for (let col = 0; col < size; col++) {
-        children.push(this.renderSquare(count++));
+        children.push(this.renderSquare(count++, size));
       }
       table.push(
         <div className="boardrow" key={row}>
@@ -63,25 +62,24 @@ class Board extends Component {
   }
 
   handleClick(i) {
-    let newBoard = [...this.state.board],
+    const { state, updateBoard, startGame } = this.props;
+    let newBoard = [...state.board],
       row = Math.floor(i / size),
       col = i % size;
 
-    console.log("handle -", i, newBoard, row, col, newBoard[row][col]);
-
     if (newBoard[row][col] !== null) return alert("Choose Another Box!");
-    newBoard[row][col] = this.state.oneNext ? theme.one : theme.two;
-    this.setState({ board: newBoard, oneNext: !this.state.oneNext });
-    // if (calcWinner(row, col, newBoard[row][col], this.state.board)) {
-    //   alert(`${newBoard[row][col]} WON !`);
-    //   this.setState({ board: startBoard(size), oneNext: true });
-    //   return;
-    // }
+    newBoard[row][col] = state.oneNext ? state.theme.one : state.theme.two;
+    updateBoard({ board: newBoard, oneNext: !state.oneNext });
+    if (calcWinner(row, col, newBoard[row][col], state.board)) {
+      alert(`${newBoard[row][col]} WON !`);
+      updateBoard({ board: startGame(size), oneNext: true });
+    }
   }
 
   render() {
+    const { state } = this.props;
     const status =
-      "Next Player: " + (this.state.oneNext ? theme.one : theme.two);
+      "Next Player: " + (state.oneNext ? state.theme.one : state.theme.two);
 
     return (
       <div className="boardFullDiv">
@@ -96,12 +94,46 @@ class Board extends Component {
 class Game extends Component {
   constructor() {
     super();
+    this.state = {
+      board: this.startGame(size),
+      oneNext: true,
+      theme: this.selectTheme()
+    };
+    this.startGame = this.startGame.bind(this);
+    this.selectTheme = this.selectTheme.bind(this);
+    this.updateBoard = this.updateBoard.bind(this);
     this.clearGame = this.clearGame.bind(this);
+    this.sizeValue = this.sizeValue.bind(this);
+  }
+
+  sizeValue() {
+    const size = document.getElementById("num-columns").value;
+    console.log("size --", size);
+    return size;
+  }
+
+  selectTheme(one = "X", two = "O") {
+    return {
+      one: one,
+      two: two
+    };
+  }
+
+  startGame(size) {
+    const newBoard = Array(size)
+      .fill(null)
+      .map(() => Array(size).fill(null));
+    this.setState();
+    return newBoard;
+  }
+
+  updateBoard(newState) {
+    this.setState({ board: newState.board, oneNext: newState.oneNext });
   }
 
   clearGame() {
     this.setState({
-      board: startBoard,
+      board: this.startGame(size),
       oneNext: true
     });
   }
@@ -110,24 +142,44 @@ class Game extends Component {
     return (
       <div className="gameFullDiv">
         <div className="gameHeader">
-          Board Size:
-          <input
-            id="num-columns"
-            name="size"
-            type="text"
-            maxlengh="2"
-            placeholder="5"
-            // value={this}
-            onFocus={e => (e.target.placeholder = "")}
-            onBlur={e => (e.target.placeholder = "5")}
-          />
-          <button id="startBtn">Start Game</button>
-          <button id="clearBtn">Clear Game</button>
+          <div id="boardSizeDiv">
+            Board Size:{" "}
+            <input
+              id="num-columns"
+              name="size"
+              type="text"
+              maxlengh="2"
+              placeholder="3"
+              // value={this}
+              onFocus={e => (e.target.placeholder = "")}
+              onBlur={e => (e.target.placeholder = "3")}
+            />
+          </div>
+          <button id="startBtn" onClick={this.sizeValue}>
+            Start Game
+          </button>
+          <button id="clearBtn" onClick={this.clearGame}>
+            Clear Game
+          </button>
+          <div id="selectDiv">
+            Player One:
+            <select value={this.state.theme.one}>
+              <option>X</option>
+              <option>O</option>
+            </select>
+          </div>
         </div>
+
         <div className="game">
           <div className="game-board">
-            <Board clearGame={this.clearGame} />
+            <Board
+              state={this.state}
+              startGame={this.startGame}
+              updateBoard={this.updateBoard}
+              size={this.sizeValue}
+            />
           </div>
+
           <div className="game-info">
             <h3>Tracker Board: </h3>
             <h5>Status: </h5>
@@ -139,7 +191,7 @@ class Game extends Component {
   }
 }
 
-// ----------------- HELPER ----------------- //
+// ----------------- HELPER FUNCTIONS ----------------- //
 function calcWinner(row, col, player, board) {
   let count = 1;
   // ROW WIN
@@ -166,7 +218,7 @@ function calcWinner(row, col, player, board) {
   // UP DIAGONAL WIN
   for (let i = 1; i < size; i++) {
     if (board[row - i] && board[row - i][col + i] == player) count++;
-    if (board[row - i] && board[row + i][col - i] == player) count++;
+    if (board[row + i] && board[row + i][col - i] == player) count++;
   }
   if (count === size) return true;
   else return false;
